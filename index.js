fbb5527591c7d12cb7c1067610039ddb64c160c8
Louis Perrenot <louis.perrenot@edu.devinci.net>
2024-05-15 16:10:08 +0200

    Add codecov

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ import {exercice1} from './src/exercice1.js';
 import {exercice2} from "./src/exercice2.js";
 import {exercice3} from "./src/exercice3.js";
 import {exercice4} from "./src/exercice4.js"; // Make sure to include the extension if "type": "module" is set
-import { CodeCovOpenTelemetry } from '@codecov/node-codecov-opentelemetry';
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { SpanKind } from '@opentelemetry/api';
-
-const sampleRate = 1;
-const untrackedExportRate = 1;
-const code = 'production::v0.0.1'; //<environment>::<versionIdentifier>
-const provider = new NodeTracerProvider();
-provider.register();
-
-const codecov = new CodeCovOpenTelemetry({
-    repositoryToken: "0471f197-8230-4bc5-a8f6-4fefdd8ae903", //from repository settings page on Codecov.
-    environment: "production", //or others as appropriate
-    versionIdentifier: "v0.0.2", //semver, commit SHA, etc
-    filters: {
-        allowedSpanKinds: [SpanKind.SERVER],
-    },
-    codecovEndpoint: "api.codecov.io",
-    sampleRate,
-    untrackedExportRate,
-    code
-});
+// import { CodeCovOpenTelemetry } from '@codecov/node-codecov-opentelemetry';
+// import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+// import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+// import { SpanKind } from '@opentelemetry/api';
+//
+// const sampleRate = 1;
+// const untrackedExportRate = 1;
+// const code = 'production::v0.0.1'; //<environment>::<versionIdentifier>
+// const provider = new NodeTracerProvider();
+// provider.register();
+//
+// const codecov = new CodeCovOpenTelemetry({
+//     repositoryToken: "0471f197-8230-4bc5-a8f6-4fefdd8ae903", //from repository settings page on Codecov.
+//     environment: "production", //or others as appropriate
+//     versionIdentifier: "v0.0.2", //semver, commit SHA, etc
+//     filters: {
+//         allowedSpanKinds: [SpanKind.SERVER],
+//     },
+//     codecovEndpoint: "api.codecov.io",
+//     sampleRate,
+//     untrackedExportRate,
+//     code
+// });
 
 const app = express();
 
