169cac7b99d1fc459eb661f1ea1b102ff394a4e7
Florian PALVADEAU <florian.palvadeau@edu.devinci.fr>
2024-05-16 10:33:26 +0200

    ex1 finalDay

--- tests/strings.test.js
+++ tests/strings.test.js
@@ -4,16 +4,17 @@ describe("The string package", () => {
   describe("the sayHelloTo function", () => {
     it("should return 'Hi, Peter!' if the argument is 'Peter'", () => {
       const actual = sayHelloTo("Peter");
-      const expected = "Hi, Peter !";
+      const expected = "Hi, Peter!";
       expect(actual).toBe(expected);
     });
   });
 
-// #
-// ##
-// ### exercice 0
-// ##
-// #
-// Make new describe
-// no data in argument error TDD
+
+  describe('the sayHelloTo function', () => {
+    test('throws error when called with no arguments', () => {
+      expect(() => {
+        yourFunction();
+      }).toThrow(Error);
+    });
+  });
 });
